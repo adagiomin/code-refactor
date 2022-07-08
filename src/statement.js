e38205e1a0a8b941bf99049fde20d356de74fef6
liumin16 <liumin16@corp.netease.com>
2022-07-08 21:31:39 +0800

    feat: 移除具有局部作用域的临时变量

--- src/statement.js
+++ src/statement.js
@@ -3,32 +3,36 @@ const plays = require("./plays.json");
 
 console.log(statement(invoices[0], plays));
 
-function amountFor(aPerformance, play) {
-  let thisAmount = 0;
-  switch (play.type) {
-    case "tragedy":
-      thisAmount = 40000;
-      if (aPerformance.audience > 30) {
-        thisAmount += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      thisAmount = 30000;
-      if (aPerformance.audience > 20) {
-        thisAmount += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      thisAmount += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${play.type}`);
-  }
-  return thisAmount;
-}
-
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance) {
+    let thisAmount = 0;
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        thisAmount = 40000;
+        if (aPerformance.audience > 30) {
+          thisAmount += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (aPerformance.audience > 20) {
+          thisAmount += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        thisAmount += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+    }
+    return thisAmount;
+  }
 
   const format = new Intl.NumberFormat("en-US", {
     style: "currency",
@@ -37,16 +41,15 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = amountFor(perf, play);
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5); // print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5); // print line for this order
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
-    totalAmount += thisAmount;
+    totalAmount += amountFor(perf);
   }
 
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
