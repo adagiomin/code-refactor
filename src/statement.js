c5bdf8ab7171234ac575db7c33af20aaf6f3bea1
liumin16 <liumin16@corp.netease.com>
2022-07-14 10:56:52 +0800

    feat: 拆分与复用 消费方式+1

--- src/statement.js
+++ src/statement.js
@@ -1,16 +1,17 @@
 const invoices = require("./invoices.json");
 const plays = require("./plays.json");
 
-console.log(statement(invoices[0], plays));
+console.log(htmlStatement(invoices[0], plays));
+console.log(plainTextStatement(invoices[0], plays));
 
-function statement(invoice, plays) {
+function createStatementData(invoice, plays) {
   // 中转数据结构，所有计算函数被挪到statement函数中，让renderPlainText只操作传进去的数据
   const result = {};
   result.customer = invoice.customer;
   result.performances = invoice.performances.map(enrichPerformance);
   result.totalAmount = totalAmount(result);
   result.totalVolumeCredits = totalVolumeCredits(result);
-  return renderPlainText(result);
+  return result;
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance); // 不想修改传给函数的参数
@@ -63,6 +64,11 @@ function statement(invoice, plays) {
   }
 }
 
+// 消费方式一
+function plainTextStatement(invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays));
+}
+
 function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
 
@@ -75,13 +81,32 @@ function renderPlainText(data) {
   result += `Amount owed is ${usd(data.totalAmount / 100)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
+}
 
-  // 震惊，这些还能写在result下面？？？
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber);
+// 消费方式二
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
   }
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+  return result;
+}
+
+// utils
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber);
 }
